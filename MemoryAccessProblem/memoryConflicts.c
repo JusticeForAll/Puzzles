245092f6cf0da41838c5fb83ba1d7c348aeef875
JusticeForAll <justice_for_all_91@hotmail.com>
2015-07-05 20:09:31 -0500

    Removed comments

--- MemoryAccessProblem/memoryConflicts.c
+++ MemoryAccessProblem/memoryConflicts.c
@@ -5,7 +5,6 @@ int SIZE = 8;
 
 int main()
 {
-    int i;
     memoryAccessEntry entries[SIZE];
     entries[0] = makeMemoryAccessEntry(1, 512, 8, READ);
     entries[1] = makeMemoryAccessEntry(2, 432, 7, WRITE);
@@ -15,13 +14,8 @@ int main()
     entries[5] = makeMemoryAccessEntry(6, 932, 3, READ);
     entries[6] = makeMemoryAccessEntry(7, 835, 2, READ);
     entries[7] = makeMemoryAccessEntry(8, 432, 1, READ);
-    
-    //for(i = 0; i < SIZE; i++)
-        //printMemoryAccessEntry(entries[i]);
-    //printf("Sorting....\n\n");
+
     findConflicts(entries, sizeof(entries)/sizeof(entries[0]));
     
-    //for(i = 0; i < SIZE; i++)
-        //printMemoryAccessEntry(entries[i]);
     return 0;
 }
